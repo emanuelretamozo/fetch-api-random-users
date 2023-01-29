294db2c819d3446c617c76819226ef1265f2c1cf
Emanuel <eretamozov@gmail.com>
2023-01-29 17:31:32 -0500

    deploying example project

---
+++ controllers/client.controller.js
@@ -0,0 +1,30 @@
+
+const url = "https://randomuser.me/api/?results=15&inc=name,dob,gender,email,nat,picture";
+
+let tableData = "";
+
+fetch(url)
+    .then(response => response.json())
+    .then(data => {
+
+        for (let i = 0; i <= data.info.results; i++) {
+            let element = document.getElementById("tableBody");
+            element.innerHTML +=  `
+                    <tbody>
+                        <tr>
+                            <td>${data.results[i].name.first}</td>
+                            <td>${data.results[i].name.last}</td>
+                            <td>${data.results[i].dob.age}</td>
+                            <td>${data.results[i].gender}</td>
+                            <td>${data.results[i].email}</td>
+                            <td>
+                                <img src="${data.results[i].picture.thumbnail}"/>
+                            </td>
+                        </tr>
+                    </tbody>
+            `
+        }
+        console.log(data);
+    });
+
+
